3ad681163dbf6a3c86e0306b3ef352325af6a0c8
Oleg <oruchkin@bk.ru>
2024-05-26 21:28:17 +0300

    fix settings

--- file_service/src/core/settings.py
+++ file_service/src/core/settings.py
@@ -2,13 +2,12 @@ import os
 
 
 class Settings:
-    # MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "minio:9000")
     MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "localhost:9000")
     MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "practicum")
     MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "StrongPass")
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost:5432/file_db")
-    DB_QUERY_LOGGING = os.getenv("DB_QUERY_LOGGING", False)
 
+    POSTGRES_URL = os.getenv("POSTGRES_URL", "postgresql+asyncpg://user:password@localhost:5432/file_db")
+    POSTGRES_QUERY_LOGGING = os.getenv("POSTGRES_QUERY_LOGGING", False)
     MINIO_USE_SSL = os.getenv("MINIO_USE_SSL", False)
 
 
--- file_service/src/db/db.py
+++ file_service/src/db/db.py
@@ -2,8 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from src.core.settings import settings
 
 
-engine = create_async_engine(settings.DATABASE_URL,
-                             echo=settings.DB_QUERY_LOGGING)
+engine = create_async_engine(settings.POSTGRES_URL,
+                             echo=settings.POSTGRES_QUERY_LOGGING)
 
 SessionLocal = async_sessionmaker(
     bind=engine,
