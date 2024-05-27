1d0faf3122fe1ca3a56fe6a4ad48119960512039
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-05-26 21:33:16 -0400

    Fix functional test

--- api/src/core/lifecycle.py
+++ api/src/core/lifecycle.py
@@ -10,22 +10,6 @@ from redis.asyncio import Redis
 logger = logging.getLogger(__name__)
 
 
-async def check_elasticsearch_connection(elastic_client) -> None:
-    try:
-        if await elastic_client.ping():
-            logger.warning("Успешное подключение к Elasticsearch.")
-    except Exception as e:
-        logger.error(f"Ошибка подключения к Elasticsearch: {e}")
-
-
-async def check_redis_connection(redis_client) -> None:
-    try:
-        if await redis_client.ping():
-            logger.warning("Успешное подключение к Redis.")
-    except Exception as e:
-        logger.error(f"Ошибка подключения к Redis: {e}")
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """
@@ -36,9 +20,6 @@ async def lifespan(app: FastAPI):
     elastic.es = AsyncElasticsearch(hosts=[elastic_settings.url])
     redis_settings = RedisSettings()
     redis.redis = Redis(host=redis_settings.host, port=redis_settings.port, decode_responses=True)
-
-    await check_elasticsearch_connection(elastic.es)
-    await check_redis_connection(redis.redis)
 
     yield
 
--- api/src/core/settings.py
+++ api/src/core/settings.py
@@ -8,5 +8,5 @@ class RedisSettings(BaseSettings):
 
 
 class ElasticsearchSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="ES_")
+    model_config = SettingsConfigDict(env_prefix="ELASTIC_")
     url: str = ""
