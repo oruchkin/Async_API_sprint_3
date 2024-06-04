66553842a025ffdb3522ec431f9c7c61b21d3a42
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-06-03 22:26:53 -0400

    bunch of fixes

--- file_service/src/models/file_model.py
+++ file_service/src/models/file_model.py
@@ -1,30 +1,29 @@
-from sqlalchemy import Column, String, Integer, DateTime, Index
+import uuid
+from datetime import UTC, datetime
+
+from sqlalchemy import DateTime, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
-import uuid
-from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
 
 Base = declarative_base()
 
 
 class FileDbModel(Base):
-    __tablename__ = 'files'
+    __tablename__ = "files"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    path_in_storage = Column(String(255), nullable=False, unique=True)
-    filename = Column(String(255), nullable=False)
-    size = Column(Integer, nullable=False)
-    file_type = Column(String(100), nullable=True)
-    short_name = Column(String(24), nullable=False, unique=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    bucket = Column(String(255), nullable=False)
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    path_in_storage: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    filename: Mapped[str] = mapped_column(String(255), nullable=False)
+    size: Mapped[int] = mapped_column(Integer, nullable=False)
+    file_type: Mapped[str] = mapped_column(String(100), nullable=True)
+    short_name: Mapped[str] = mapped_column(String(24), nullable=False, unique=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(UTC))
+    bucket: Mapped[str] = mapped_column(String(255), nullable=False)
 
-    def __init__(self, path_in_storage: str,
-                 filename: str,
-                 short_name: str,
-                 size: int,
-                 file_type: str,
-                 bucket: str) -> None:
+    def __init__(
+        self, path_in_storage: str, filename: str, short_name: str, size: int, file_type: str, bucket: str
+    ) -> None:
         self.path_in_storage = path_in_storage
         self.filename = filename
         self.short_name = short_name
@@ -33,4 +32,4 @@ class FileDbModel(Base):
         self.bucket = bucket
 
     def __repr__(self) -> str:
-        return f'<id {self.id}>'
+        return f"<id {self.id}>"
--- file_service/tests/test_files.py
+++ file_service/tests/test_files.py
@@ -1,23 +1,22 @@
 import random
-from unittest.mock import AsyncMock, MagicMock
-import pytest
+import uuid
+from unittest.mock import AsyncMock
+
 from faker import Faker
 from fastapi import UploadFile
-from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.responses import StreamingResponse
-import uuid
+from src.db.base_provider import BaseProvider
 from src.models.file_model import FileDbModel
 from src.services.file_service import FileService
 from src.services.minio_service import MinioStorage
+from starlette.responses import StreamingResponse
 
 fake = Faker()
 
 mime_types = ["audio/mpeg", "audio/ogg", "video/mp4", "video/x-msvideo", "video/quicktime"]
 
-pytestmark = pytest.mark.asyncio
 
 async def test_upload_file():
-    mock_db = AsyncMock(spec=AsyncSession)
+    mock_db = AsyncMock(spec=BaseProvider)
     mock_storage = AsyncMock(spec=MinioStorage)
     mock_upload_file = AsyncMock(spec=UploadFile)
 
@@ -28,17 +27,14 @@ async def test_upload_file():
 
     file_service = FileService(db=mock_db, storage=mock_storage)
 
-    path = f"uploads/{mock_upload_file.filename}"
-
-    file_service.generate_unique_path = AsyncMock(return_value=path)
-
     await file_service.upload_file(file=mock_upload_file, bucket="mybucket", path=mock_upload_file.filename)
 
     mock_storage.save.assert_called_once()
-    mock_db.commit.assert_called_once()
+    mock_db.add.assert_called_once()
+
 
 async def test_download_file():
-    mock_db = AsyncMock(spec=AsyncSession)
+    mock_db = AsyncMock(spec=BaseProvider)
     mock_storage = AsyncMock(spec=MinioStorage)
     file_service = FileService(db=mock_db, storage=mock_storage)
 
@@ -48,40 +44,38 @@ async def test_download_file():
         short_name=str(uuid.uuid4()),
         size=fake.random_number(digits=6),
         file_type=random.choice(mime_types),
-        bucket="mybucket"
+        bucket="mybucket",
     )
 
-    mock_result = MagicMock()
-    mock_result.scalars.return_value.first.return_value = file_db
-    mock_db.execute.return_value = mock_result
+    mock_db.find_by_shortname.return_value = file_db
 
-    mock_storage.get_file.return_value = StreamingResponse(iter([b'test data']))
+    mock_storage.get_file.return_value = StreamingResponse(iter([b"test data"]))
 
-    result = await file_service.download_file(bucket="mybucket", short_name=file_db.short_name)
+    result = await file_service.download_file(short_name=file_db.short_name)
 
-    mock_storage.get_file.assert_called_once_with("mybucket", file_db.path_in_storage)
+    mock_storage.get_file.assert_called_once_with(file_db.bucket, file_db.path_in_storage)
     assert isinstance(result, StreamingResponse)
 
-async def test_delete_file():
-    mock_db = AsyncMock(spec=AsyncSession)
-    mock_storage = AsyncMock(spec=MinioStorage)
-    file_service = FileService(db=mock_db, storage=mock_storage)
 
+async def test_delete_file():
     file_db = FileDbModel(
         path_in_storage=f"uploads/{fake.file_name()}",
         filename=fake.file_name(),
         short_name=str(uuid.uuid4()),
         size=fake.random_number(digits=6),
         file_type=random.choice(mime_types),
-        bucket="mybucket"
+        bucket="mybucket",
     )
 
-    mock_result = MagicMock()
-    mock_result.scalars.return_value.first.return_value = file_db
-    mock_db.execute.return_value = mock_result
+    mock_db = AsyncMock(spec=BaseProvider)
+    mock_db.find_by_shortname.return_value = file_db
 
-    await file_service.delete_file(bucket="mybucket", short_name=file_db.short_name)
+    mock_storage = AsyncMock(spec=MinioStorage)
+    file_service = FileService(db=mock_db, storage=mock_storage)
 
+    # act
+    await file_service.delete_file(short_name=file_db.short_name)
+
+    # assert
     mock_storage.delete.assert_called_once_with("mybucket", file_db.path_in_storage)
     mock_db.delete.assert_called_once_with(file_db)
-    mock_db.commit.assert_called_once()
