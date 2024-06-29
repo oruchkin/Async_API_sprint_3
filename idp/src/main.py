079959a8610b6ba971995f283da41cc40f0ef8a7
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-06-29 09:39:44 -0400

    Improve errors handling

--- idp/src/main.py
+++ idp/src/main.py
@@ -1,12 +1,9 @@
-from http import HTTPStatus
-
-import services.errors as errors
 import uvicorn
 from api.v1 import roles, users
+from core.errors_utils import error_to_json_response
 from core.lifecycle import lifespan
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
 
 load_dotenv()
 
@@ -22,28 +19,10 @@ app = FastAPI(
 
 @app.exception_handler(Exception)
 async def unicorn_exception_handler(request: Request, error: Exception):
-    if isinstance(error, errors.NotAuthorizedError):
-        return JSONResponse(
-            status_code=HTTPStatus.UNAUTHORIZED,
-            content={"message": error.message},
-        )
-
-    if isinstance(error, errors.NotFoundError):
-        return JSONResponse(
-            status_code=HTTPStatus.NOT_FOUND,
-            content={"message": error.message},
-        )
-
-    if isinstance(error, errors.ValidationError):
-        return JSONResponse(
-            status_code=HTTPStatus.BAD_REQUEST,
-            content={"message": error.message},
-        )
-
-    return JSONResponse(
-        status_code=418,
-        content={"message": "Something went wrong... it's time to check the logs"},
-    )
+    """
+    Wraps api errors into the valid json responses
+    """
+    return error_to_json_response(error)
 
 
 app.include_router(users.router, prefix="/api/v1/users", tags=["users"])
