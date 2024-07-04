b7ace28c7ae8c6559c18264f40a7e6e2b31ec2d5
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-07-04 15:50:56 -0400

    Add elasticsearch and redis into trace

--- api/src/services/cache/redis_storage.py
+++ api/src/services/cache/redis_storage.py
@@ -1,8 +1,11 @@
 from typing import Any
 
+from opentelemetry import trace
 from redis.asyncio import Redis
 
 from .storage import ICache
+
+tracer = trace.get_tracer(__name__)
 
 
 class RedisCache(ICache):
@@ -10,7 +13,9 @@ class RedisCache(ICache):
         self._client = client
 
     async def set(self, key: str, value: Any, timeout_sec: int) -> None:
-        await self._client.set(key, value, timeout_sec)
+        with tracer.start_as_current_span("redis-request"):
+            await self._client.set(key, value, timeout_sec)
 
     async def get(self, key: str) -> Any:
-        return await self._client.get(key)
+        with tracer.start_as_current_span("redis-request"):
+            return await self._client.get(key)
