207ef46895d377645f9d054a44fbd089669a657a
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-07-06 21:35:33 -0400

    Load user in middleware

--- idp/src/main.py
+++ idp/src/main.py
@@ -1,11 +1,15 @@
 import uvicorn
 from api.v1 import roles, users
+from core.auth import BasicAuthBackend
 from core.errors_utils import error_to_json_response
 from core.lifecycle import lifespan
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
+from starlette.middleware.authentication import AuthenticationMiddleware
 
 load_dotenv()
+
+middleware = []
 
 app = FastAPI(
     title="Identity Provider API",
@@ -14,6 +18,7 @@ app = FastAPI(
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     lifespan=lifespan,
+    middleware=middleware,
 )
 
 
@@ -24,6 +29,17 @@ async def unicorn_exception_handler(request: Request, error: Exception):
     """
     return error_to_json_response(error)
 
+
+@app.middleware("http")
+async def throttle_by_user(request: Request, call_next):
+    if request.user.is_authenticated:
+        # throttle here
+        pass
+
+    return await call_next(request)
+
+
+app.add_middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
 
 app.include_router(users.router, prefix="/api/v1/users", tags=["users"])
 app.include_router(roles.router, prefix="/api/v1/roles", tags=["roles"])
