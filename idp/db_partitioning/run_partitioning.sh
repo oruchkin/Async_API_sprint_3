14f8d8c2ae54cf49e121891f3be86f9603f0f680
Oleg <oruchkin@bk.ru>
2024-07-09 00:29:19 +0300

    db_partitioning

--- idp/db_partitioning/run_partitioning.sh
+++ idp/db_partitioning/run_partitioning.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Путь к файлу .env на уровень выше
 ENV_FILE="../.env"
 
-# Загрузка переменных из .env файла
 export $(grep -v '^#' $ENV_FILE | xargs)
 
-# Переопределение POSTGRES_HOST для локального выполнения
 POSTGRES_HOST="localhost"
 POSTGRES_DB="postgres"
 
-# Выполнение SQL-скрипта
 PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -f partitioning.sql
 
 if [ $? -eq 0 ]; then
