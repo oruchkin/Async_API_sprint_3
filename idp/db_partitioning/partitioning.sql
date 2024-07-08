822d508c1e41011c3487a215417af1ab052ce1cc
Oleg <oruchkin@bk.ru>
2024-07-09 00:28:40 +0300

    db_partitioning

--- idp/db_partitioning/partitioning.sql
+++ idp/db_partitioning/partitioning.sql
@@ -1,7 +1,5 @@
--- Переименование существующей таблицы
 ALTER TABLE user_entity RENAME TO user_entity_old;
 
--- Создание новой основной таблицы с партиционированием
 CREATE TABLE user_entity (
     id                          varchar(36)           not null,
     email                       varchar(255),
@@ -21,10 +19,8 @@ CREATE TABLE user_entity (
     UNIQUE (realm_id, username, created_timestamp)
 ) PARTITION BY RANGE (created_timestamp);
 
--- Назначение владельца таблицы
 ALTER TABLE user_entity OWNER TO app;
 
--- Создание партиций
 CREATE TABLE user_entity_2022 PARTITION OF user_entity
     FOR VALUES FROM (1640995200) TO (1672531199);  -- Unix timestamp for 2022
 
@@ -40,16 +36,13 @@ CREATE TABLE user_entity_2025 PARTITION OF user_entity
 CREATE TABLE user_entity_2026 PARTITION OF user_entity
     FOR VALUES FROM (1767139200) TO (1798675199);  -- Unix timestamp for 2026
 
--- Перенос данных в партиции
 INSERT INTO user_entity SELECT * FROM user_entity_old WHERE created_timestamp BETWEEN 1640995200 AND 1672531199;
 INSERT INTO user_entity SELECT * FROM user_entity_old WHERE created_timestamp BETWEEN 1672531200 AND 1704067199;
 INSERT INTO user_entity SELECT * FROM user_entity_old WHERE created_timestamp BETWEEN 1704067200 AND 1735603199;
 INSERT INTO user_entity SELECT * FROM user_entity_old WHERE created_timestamp BETWEEN 1735603200 AND 1767139199;
 INSERT INTO user_entity SELECT * FROM user_entity_old WHERE created_timestamp BETWEEN 1767139200 AND 1798675199;
 
--- Воссоздание индексов
 CREATE INDEX idx_user_email ON user_entity (email);
 CREATE INDEX idx_user_service_account ON user_entity (realm_id, service_account_client_link);
 
--- Удаление старой таблицы вместе с зависимостями
 DROP TABLE user_entity_old CASCADE;
