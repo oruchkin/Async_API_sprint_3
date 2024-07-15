3225da3e9e98401aae536b39ac22caa87b4d42e0
Andrew <av.grebennikov@gmail.com>
2024-07-15 08:50:09 -0400

    Adjust idp startup (#72)
    Adjust docker compose profile to make auth work with `http://localhost:29543/api/v1/auth/vk`
    Start with `docker compose --profile idp up`

--- idp/src/core/lifecycle.py
+++ idp/src/core/lifecycle.py
@@ -2,8 +2,6 @@ import logging.config
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from services.keycloak_client import KeycloackClient, get_keycloak_service
-from services.oidc_client import get_oidc_service
 
 logger = logging.getLogger(__name__)
 
@@ -11,9 +9,6 @@ logger = logging.getLogger(__name__)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info("Starting...")
-
-    oidc = get_oidc_service()
-    keycloak: KeycloackClient = get_keycloak_service(oidc)
 
     yield
 
