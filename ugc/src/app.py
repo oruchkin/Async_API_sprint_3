253a42c02977076822f0cfb0920c416eb567b0d4
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-07-27 21:34:43 -0400

    Add swagger

--- ugc/src/app.py
+++ ugc/src/app.py
@@ -1,10 +1,42 @@
+from flasgger import Swagger
 from flask import Flask
 
 app = Flask(__name__)
 
+# Swagger available at /apidocs/
+swagger = Swagger(app)
+
 
 @app.route('/hello-world')
 def hello_world():
+    """Example endpoint returning a list of colors by palette
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: palette
+        in: path
+        type: string
+        enum: ['all', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
+    responses:
+      200:
+        description: A list of colors (may be filtered by palette)
+        schema:
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
     return 'Hello, World!'
 
 
