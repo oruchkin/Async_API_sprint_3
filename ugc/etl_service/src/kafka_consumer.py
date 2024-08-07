420386c1a4be624dbba0c05c52909f89050162c8
Oleg <oruchkin@bk.ru>
2024-08-07 23:30:56 +0300

    reworked etl for clichkouse, kafka

--- ugc/etl_service/src/kafka_consumer.py
+++ ugc/etl_service/src/kafka_consumer.py
@@ -2,22 +2,34 @@ from kafka import KafkaConsumer
 import json
 from transformers import transform_data
 from clickhouse_client import ClickHouseClient
+import logging
+
 
 class KafkaConsumerService:
-    def __init__(self, kafka_server, topic, group_id):
-        self.consumer = KafkaConsumer(
+    def __init__(self, kafka_server: str, clickhouse_server: str, topic: str, group_id: str):
+        self.kafka_consumer = KafkaConsumer(
             topic,
             bootstrap_servers=kafka_server,
             auto_offset_reset='earliest',
-            enable_auto_commit=True,
+            enable_auto_commit=False,
             group_id=group_id
         )
-        self.clickhouse_client = ClickHouseClient()
+        self.clickhouse_client = ClickHouseClient(clickhouse_server)
+        logging.basicConfig(level=logging.INFO)
 
     def consume_messages(self):
-        for message in self.consumer:
+        messages = []
+        # чтобы в базу попали значения нужно отправить минимум batch_to_insert_db задач
+        batch_to_insert_db = 5
+
+        for message in self.kafka_consumer:
             data = json.loads(message.value.decode('utf-8'))
+            logging.info(f"Received data from kafka: {data}")
             transformed_data = transform_data(data)
-            print("transformed_data")
-            print(transformed_data)
-            self.clickhouse_client.insert_data(transformed_data)
+
+            messages.append(transformed_data)
+
+            if len(messages) >= batch_to_insert_db:
+                self.clickhouse_client.insert_data(messages)
+                self.kafka_consumer.commit()
+                messages = []
--- ugc/etl_service/src/transformers.py
+++ ugc/etl_service/src/transformers.py
@@ -1,8 +1,4 @@
-from datetime import datetime
-
 def transform_data(data):
-    print("hey")
-    print(data)
     return {
         "user_id": data["user_id"],
         "movie_id": data["movie_id"],
