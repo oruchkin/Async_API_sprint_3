a9786f11458e942bc2e9ef6f0067cb24338a7731
Andrew <av.grebennikov@gmail.com>
2024-11-23 19:29:42 -0500

    Fix build  script

--- infra/k8s/minikube-windows-build.cmd
+++ infra/k8s/minikube-windows-build.cmd
@@ -1,24 +1,24 @@
-SET DOCKER_TLS_VERIFY=1
-SET DOCKER_HOST=tcp://127.0.0.1:51071
-SET DOCKER_CERT_PATH=C:\Users\avgre\.minikube\certs
-SET MINIKUBE_ACTIVE_DOCKERD=minikube
+@echo off
+REM Set script to fail on error
+setlocal enabledelayedexpansion
+
 REM To point your shell to minikube's docker-daemon, run:
-REM @FOR /f "tokens=*" %i IN ('minikube -p minikube docker-env --shell cmd') DO @%i
+FOR /f "tokens=*" %%i IN ('minikube -p minikube docker-env --shell cmd') DO %%i
 
-docker build -t k8s-keycloak ../idp/keycloak
+docker build -t k8s-keycloak ../../idp/keycloak
 
-docker build -t k8s-admin ../admin
+docker build -t k8s-admin ../../admin
 
-docker build -t k8s-admin-nginx ../admin -f ../admin/nginx/Dockerfile
+docker build -t k8s-admin-nginx ../../admin -f ../../admin/nginx/Dockerfile
 
-docker build -t k8s-idp ../idp
+docker build -t k8s-idp ../../idp
 
-docker build -t k8s-pginit ../postgres_init
+docker build -t k8s-pginit ../../postgres_init
 
-docker build -t k8s-etl ../etl
+docker build -t k8s-etl ../../etl
 
-docker build -t k8s-fileapi ../file_service
+docker build -t k8s-fileapi ../../file_service
 
-docker build -t k8s-api ../api
+docker build -t k8s-api ../../api
 
-docker build -t k8s-notifications ../notifications
+docker build -t k8s-notifications ../../notifications
